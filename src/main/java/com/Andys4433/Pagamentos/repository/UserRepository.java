dcd28cb4cd8aa7349a8c718fdb43f2e9489f2cc1
Andy4433 <anderson.s.brito613@gmail.com>
2024-01-22 17:54:28 -0300

    correçao em alguns arquivos

--- src/main/java/com/Andys4433/Pagamentos/repository/UserRepository.java
+++ src/main/java/com/Andys4433/Pagamentos/repository/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.Andys4433.Pagamentos.entity.User;
-import java.util.List;
+
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
